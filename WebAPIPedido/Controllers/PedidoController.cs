6327ad7b070107f6815924c02c9db460ccea6c3d
hugo1623 <hugoluis.system@gmail.com>
2021-06-07 23:21:41 -0500

    Creacion de PedidoController y de la entidad Pedido

---
+++ WebAPIPedido/Controllers/PedidoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIPedido.Entidades;
+
+namespace WebAPIPedido.Controllers
+{
+    [ApiController]
+    [Route("api/pedidos")]
+    public class PedidoController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Pedido>> Get()
+        {
+            return new List<Pedido>()  { 
+                new Pedido() { IdPedido = 1, Estado = "Pagado", Detalle = " Producto de Limpieza" },
+                new Pedido() { IdPedido = 2, Estado = "No Pagado", Detalle = " Producto de Cocina" } };
+        }
+
+
+    }
+}
---
+++ WebAPIPedido/Entidades/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIPedido.Entidades
+{
+    public class Pedido
+    {
+        public int IdPedido { get; set; }
+        public DateTime Fecha_Pedido { get; set; }
+        public DateTime Fecha_Espera { get; set; }
+        public DateTime Fecha_Entrega { get; set; }
+        public string  Estado { get; set; }
+        public string Detalle { get; set; }
+    }
+}
